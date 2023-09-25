e914ecae307edf17b27062b004de1f8aa57fc510
br08 <brayan.neves@gmail.com>
2023-09-25 18:51:05 -0300

    refactor: Refactor installation script.
    No more cloning the whole repository. Just run the installation script, answer the prompts and the rest will be taken care of.

--- install.sh
+++ install.sh
@@ -1,6 +1,32 @@
 #!/usr/bin/env bash
 
-INVALID="Invalid option!"
+INVALID_OPTION="Invalid option!"
+
+branch="main"
+
+root="https://raw.githubusercontent.com/br08/react-boilerplate-project/$branch"
+scripts="${root}/scripts"
+templates="${root}/templates"
+tsconfig="tsconfig.json"
+webpack_config="webpack.config.js"
+dir="javascript"
+index="index.js"
+app="App.js"
+
+files=()
+src=()
+
+config_javascript() {
+  dev_dep+=($babel)
+}
+
+config_typescript() {
+  dev_dep+=($typescript)
+  dir="typescript"
+  index="index.tsx"
+  app="App.tsx"
+  files+=($templates/$dir/$tsconfig)
+}
 
 replace_spc() { #replaces spaces for underscores
   str=$1
@@ -13,11 +39,13 @@ replace_uds() { # replaces underscores for spaces
 }
 
 use_yarn() {
-  pak_install="yarn add"
+  pakman="yarn"
+  install_cmd="yarn add"
 }
 
 use_npm() {
-  pak_install="npm i"
+  pakman="npm"
+  install_cmd="npm i"
 }
 
 warn() {
@@ -39,7 +67,7 @@ then
   case $pakman in
     y) use_yarn ;;
     n) use_npm ;;
-    *) warn "$INVALID" ;;
+    *) warn "$INVALID_OPTION" ;;
   esac
 else
   echo "Using npm as the default option."
@@ -52,39 +80,68 @@ echo ""
 webpack=$(replace_spc "webpack webpack-cli webpack-dev-server html-webpack-plugin")
 babel=$(replace_spc "@babel/core @babel/plugin-transform-runtime @babel/preset-env @babel/preset-react babel-loader")
 react=$(replace_spc "react react-dom")
+typescript=$(replace_spc "typescript @types/react @types/react-dom ts-loader")
 
-dev_dep=($webpack $babel)
+dev_dep=($webpack)
 dep=($react)
 
-echo "Installing dependencies..."
-for pak in "${dep[@]}"
-do
-  eval "${pak_install} $(replace_uds $pak)"
-  echo ""
-done
-
-echo "Installing dev dependencies..."
-for pak in "${dev_dep[@]}"
-do
-  eval "${pak_install} -D $(replace_uds $pak)"
-  echo ""
-done
-
-printf "Finished installing the packages!
-
-You won't need this installation script anymore.
-Do you wish it to be deleted?
+printf "
+Do you wish to use TypeScript? (default \"n\")
 
 y) yes
 n) no
 
 > "
-read delete
+read with_typescript
 
-if [ -n "${delete}" ]
+if [ -n "${with_typescript}" ]
 then
-  if [ "${delete}" == "y" ]
-  then
-    rm install.sh
-  fi
+  case $with_typescript in
+    y) config_typescript ;;
+    n) config_javascript ;;
+    *) warn "$INVALID_OPTION" ;;
+  esac
+else
+  echo "TypeScript will not be installed in this project."
+  use_npm
 fi
+
+files+=($templates/$dir/$webpack_config $templates/index.html)
+src+=($templates/$dir/$index $templates/$dir/$app)
+
+echo ""
+eval "${pakman} init"
+echo ""
+
+[ -r "package.json" ] && node -e "$(curl -fsSL $scripts/pk-json-scripts.js)"
+
+echo "Installing dependencies..."
+echo ""
+for pak in "${dep[@]}"
+do
+  eval "${install_cmd} $(replace_uds $pak)"
+  echo ""
+done
+
+echo "Installing dev dependencies..."
+echo ""
+for pak in "${dev_dep[@]}"
+do
+  eval "${install_cmd} -D $(replace_uds $pak)"
+  echo ""
+done
+
+echo "Finished installing the packages!"
+echo ""
+echo "Dowloading templates..."
+for file in "${files[@]}"
+do
+  curl -fsSLO "${file}"
+done
+mkdir src
+cd src
+for file in "${src[@]}"
+do
+  curl -fsSLO "${file}"
+done
+echo "Done!"
