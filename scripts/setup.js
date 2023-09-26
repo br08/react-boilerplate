b3a503eb0a36f0aeb5f98f0886ee9f65f9497417
br08 <brayan.neves@gmail.com>
2023-09-26 17:34:23 -0300

    feat: Add parameters.
    Now you can specify keys/values pairs as arguments to be added to package.json.

---
+++ scripts/setup.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const args = process.argv.slice(2);
+
+// Object to store the parameters
+const params = {};
+
+// Process the command-line arguments
+args.forEach((arg) => {
+    const [key, value] = arg.split('=');
+    if (key && value) {
+        params[key.replace('--', '')] = value;
+    }
+});
+
+const file = 'package.json';
+
+// Parse package.json to object
+const packageJson = JSON.parse(fs.readFileSync(file));
+
+// Remove some default generated properties
+const [ scripts, keywords ] = packageJson;
+if (scripts) delete packageJson.scripts;
+if (keywords) delete packageJson.keywords;
+
+// Setup scripts
+const build = 'rm -rf dist && NODE_ENV=production webpack';
+const start = 'webpack serve';
+const dev = 'webpack serve --mode development';
+
+packageJson.scripts = {};
+packageJson.scripts.build = build;
+packageJson.scripts.start = start;
+packageJson.scripts.dev = dev;
+
+// Merge params passed as arguments to packageJson
+const mergedParams = Object.assign ({}, packageJson, params)
+
+// Overwrite package.json
+fs.writeFileSync(file, JSON.stringify(mergedParams, null, 2));
