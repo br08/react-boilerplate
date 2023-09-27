4c1cf67516413a28fc8b072e7a939009270de61f
br08 <brayan.neves@gmail.com>
2023-09-27 12:18:22 -0300

    refactor: Restructure and optimize shell script. - Add improved package manager selection and project setup. - Standardize script formatting and variable naming. - Update script comments and add explanatory comments. - Clean up redundant code and optimize imports. - Correct variable assignment and input validation.

--- install.sh
+++ install.sh
@@ -1,32 +1,33 @@
 #!/usr/bin/env bash
 
+# Constants
 INVALID_OPTION="Invalid option!"
+DEFAULT_DIR=$(basename "$(pwd)")
+DEFAULT_VERSION="0.1"
+DEFAULT_LICENSE="MIT"
 
-branch="main"
+ROOT="https://raw.githubusercontent.com/br08/react-boilerplate/main"
+SCRIPTS="$ROOT/scripts"
+TEMPLATES_PATH="$ROOT/templates"
 
-root="https://raw.githubusercontent.com/br08/react-boilerplate/$branch"
-scripts="${root}/scripts"
-templates="${root}/templates"
+# Global variables
 tsconfig="tsconfig.json"
 webpack_config="webpack.config.js"
 dir="javascript"
+ext="js"
 index="index.js"
 app="App.js"
 
-files=()
+config_files=()
 src=()
 
-config_javascript() {
-  dev_dep+=($babel)
-}
+pkg_name=""
+version=""
+license=""
+description=""
+author=""
 
-config_typescript() {
-  dev_dep+=($typescript)
-  dir="typescript"
-  index="index.tsx"
-  app="App.tsx"
-  files+=($templates/$dir/$tsconfig)
-}
+# Functions
 
 replace_spc() { #replaces spaces for underscores
   str=$1
@@ -53,95 +54,182 @@ warn() {
   exit 0
 }
 
-prompt="
+choose_package_manager() {
+  prompt="
 Which package manager would you like to use? (default \"n\")
 
 y) yarn
 n) npm
 
 > "
-read -p "$prompt" pakman
+  read -p "$prompt" pakman
 
-if [ -n "${pakman}" ]
-then
-  case $pakman in
-    y) use_yarn ;;
-    n) use_npm ;;
-    *) warn "$INVALID_OPTION" ;;
-  esac
-else
-  echo "Using npm as the default option."
-  use_npm
-fi
+  if [ -n "$pakman" ]
+  then
+    case $pakman in
+      y) use_yarn ;;
+      n) use_npm ;;
+      *) warn "$INVALID_OPTION" ;;
+    esac
+  else
+    echo "Using npm as the default option."
+    use_npm
+  fi
+}
 
-echo ""
+prompt_pkg_name() {
+  prompt="package name ($DEFAULT_DIR): "
+  read -p "$prompt" pkg_name
 
-# Essential packages
-webpack=$(replace_spc "webpack webpack-cli webpack-dev-server html-webpack-plugin")
-babel=$(replace_spc "@babel/core @babel/plugin-transform-runtime @babel/preset-env @babel/preset-react babel-loader")
-react=$(replace_spc "react react-dom")
-typescript=$(replace_spc "typescript @types/react @types/react-dom ts-loader")
+  if [ -z "$pkg_name" ]; then
+    pkg_name="$DEFAULT_DIR"
+  fi
 
-dev_dep=($webpack)
-dep=($react)
+  if [ -n "$pkg_name" ] && [ "$pkg_name" != "$DEFAULT_DIR" ]
+  then
+    eval "mkdir $pkg_name"
+    eval "cd $pkg_name"
+  fi
+}
 
-prompt="
+prompt_version() {
+  prompt="version ($DEFAULT_VERSION): "
+  read -p "$prompt" version
+
+  if [ -z "$version" ]; then
+    version="$DEFAULT_VERSION"
+  fi
+}
+
+prompt_description() {
+  prompt="description: "
+  read -p "$prompt" description
+}
+
+prompt_author() {
+  prompt="author: "
+  read -p "$prompt" author
+}
+
+prompt_license() {
+  prompt="license ($DEFAULT_LICENSE): "
+  read -p "$prompt" license
+
+  if [ -z "$version" ]; then
+    license="$DEFAULT_LICENSE"
+  fi
+}
+
+setup_for_javascript() {
+  dev_dep+=($babel)
+}
+
+setup_for_typescript() {
+  dev_dep+=($typescript)
+  dir="typescript"
+  ext="tsx"
+  config_files+=($TEMPLATES_PATH/$dir/$tsconfig)
+}
+
+prompt_typescript() {
+  prompt="
 Do you wish to use TypeScript? (default \"n\")
 
 y) yes
 n) no
 
 > "
-read -p "$prompt" with_typescript
+  read -p "$prompt" with_typescript
+  
+  if [ -n "$with_typescript" ]
+  then
+    case $with_typescript in
+      y) setup_for_typescript ;;
+      n) setup_for_javascript ;;
+      *) warn "$INVALID_OPTION" ;;
+    esac
+  else
+    use_npm
+  fi
+}
 
-if [ -n "${with_typescript}" ]
-then
-  case $with_typescript in
-    y) config_typescript ;;
-    n) config_javascript ;;
-    *) warn "$INVALID_OPTION" ;;
-  esac
-else
-  echo "TypeScript will not be installed in this project."
-  use_npm
-fi
+set_templates() {
+  config_files+=($TEMPLATES_PATH/$dir/$webpack_config $TEMPLATES_PATH/index.html)
+  src+=($TEMPLATES_PATH/$dir/index.$ext $TEMPLATES_PATH/$dir/App.$ext)
+}
 
-files+=($templates/$dir/$webpack_config $templates/index.html)
-src+=($templates/$dir/$index $templates/$dir/$app)
+set_packages() {
+  webpack=$(replace_spc "webpack webpack-cli webpack-dev-server html-webpack-plugin")
+  babel=$(replace_spc "@babel/core @babel/plugin-transform-runtime @babel/preset-env @babel/preset-react babel-loader")
+  react=$(replace_spc "react react-dom")
+  typescript=$(replace_spc "typescript @types/react @types/react-dom ts-loader")
 
-echo ""
-eval "${pakman} init"
-echo ""
+  dev_dep=($webpack)
+  dep=($react)
+}
 
-[ -r "package.json" ] && node -e "$(curl -fsSL $scripts/pk-json-scripts.js)"
+install_dependencies() {
+  packages=$1
+  for pak in "${packages[@]}"
+  do
+    eval "${install_cmd} $(replace_uds $pak)"
+    echo ""
+  done
+}
 
-echo "Installing dependencies..."
-echo ""
-for pak in "${dep[@]}"
-do
-  eval "${install_cmd} $(replace_uds $pak)"
+setup() {
+  prompt_typescript
+  prompt_pkg_name
+  prompt_version
+  prompt_description
+  prompt_author
+  prompt_license
+
+  set_templates
+
   echo ""
-done
-
-echo "Installing dev dependencies..."
-echo ""
-for pak in "${dev_dep[@]}"
-do
-  eval "${install_cmd} -D $(replace_uds $pak)"
+  eval "$pakman init -ysf" > /dev/null 2> /dev/null # supress messages from both npm and yarn
   echo ""
-done
 
-echo "Finished installing the packages!"
-echo ""
-echo "Dowloading templates..."
-for file in "${files[@]}"
-do
-  curl -fsSLO "${file}"
-done
-mkdir src
-cd src
-for file in "${src[@]}"
-do
-  curl -fsSLO "${file}"
-done
-echo "Done!"
+  if [ -r "package.json" ]
+  then
+    curl -fsSLO $SCRIPTS/setup.js
+    node setup.js --name="$pkg_name" --version="$version" --description="$description" --author="$author" --license="$license" --main="index.$ext"
+    rm setup.js
+  fi
+
+  set_packages
+
+  echo "Installing dependencies..."
+  echo ""
+  install_dependencies $dep
+
+  echo "Installing dev dependencies..."
+  echo ""
+  install_dependencies $dev_dep
+
+  echo "Finished installing the dependencies!"
+  echo ""
+
+  echo "Dowloading templates..."
+  for file in "${config_files[@]}"
+  do
+    curl -fsSLO "${file}"
+  done
+  mkdir src
+  cd src
+  for file in "${src[@]}"
+  do
+    curl -fsSLO "${file}"
+  done
+  echo "Done!"
+}
+
+main() {
+  choose_package_manager
+  echo ""
+  setup
+}
+
+# main
+main
